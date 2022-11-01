fbfab050b6f5ff2441dd4b500ddb238d67c2af50
“kieuvu” <kieuvuvuvu@gmail.com>
2022-11-01 23:47:46 +0700

    user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
+import { ResponseUtility } from 'src/utils/response.utils';
 
 @Injectable()
-export class User {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(user: User) {
+    const result = await this.userRepository.create(user);
+    return new ResponseUtility().setData('user', result).setStatus(true);
+  }
+}
